f5bb856d9b56eac4d190d7daf782a40c44be9920
mmcaul <melissa.mcauley27@gmail.com>
2020-07-15 21:16:06 -0400

    pre pull NoFromMel

--- src/Client.java
+++ src/Client.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.io.*;
 import java.net.*;
 
@@ -31,13 +32,16 @@ class Client {
                     //System.out.println("size " + size);
                     input.read(aBuffer,0,size);
                     String s = new String(aBuffer).trim();
-                    jeu = new Jeu(s);
+                    //jeu = new Jeu(s);
                     System.out.println(s);
                     String[] boardValues;
                     boardValues = s.split(" ");
                     int x=0,y=0;
                     for(int i=0; i<boardValues.length;i++){
                         board[x][y] = Integer.parseInt(boardValues[i]);
+
+
+
                         x++;
                         if(x == 8){
                             x = 0;
@@ -46,8 +50,12 @@ class Client {
                     }
 
                     System.out.println("Nouvelle partie! Vous jouer blanc, entrez votre premier coup : ");
-                    String move = null;
-                    move = console.readLine();
+
+                    Noeud racine = new Noeud(board, true);
+                    Minmax minmax = new Minmax(racine);
+                    String move = minmax.findPositionChange(racine,2,true);
+
+                    //String nextMove = console.readLine();
                     output.write(move.getBytes(),0,move.length());
                     output.flush();
                 }
@@ -60,7 +68,7 @@ class Client {
                     //System.out.println("size " + size);
                     input.read(aBuffer,0,size);
                     String s = new String(aBuffer).trim();
-                    jeu = new Jeu(s);
+                    //jeu = new Jeu(s);
                     System.out.println(s);
                     String[] boardValues;
                     boardValues = s.split(" ");
@@ -84,15 +92,15 @@ class Client {
                     System.out.println("size :" + size);
                     input.read(aBuffer,0,size);
                     String s = new String(aBuffer);
-                    jeu = new Jeu(s);
+                    //jeu = new Jeu(s);
                     System.out.println("Dernier coup :"+ s);
                     //System.out.println("Entrez votre coup : ");
 
-                    Noeud racine = new Noeud(jeu.getPlateau(), true);
+                    Noeud racine = new Noeud(board, true);
                     Minmax minmax = new Minmax(racine);
                     String nextMove = minmax.findPositionChange(racine,2,true);
 
-                    //String move = "D7-C6";
+                    //String nextMove = "D7-C6";
                     //move = console.readLine();
                     output.write(nextMove.getBytes(),0,nextMove.length());
                     output.flush();
@@ -101,8 +109,13 @@ class Client {
                 // Le dernier coup est invalide
                 if(cmd == '4'){
                     System.out.println("Coup invalide, entrez un nouveau coup : ");
-                    String move = null;
-                    move = console.readLine();
+                    //String move = null;
+                    //move = console.readLine();
+
+                    Noeud racine = new Noeud(board, true);
+                    Minmax minmax = new Minmax(racine);
+                    String move = minmax.findPositionChange(racine,2,true);
+
                     output.write(move.getBytes(),0,move.length());
                     output.flush();
 
@@ -114,8 +127,13 @@ class Client {
                     input.read(aBuffer,0,size);
                     String s = new String(aBuffer);
                     System.out.println("Partie Terminé. Le dernier coup joué est: "+s);
-                    String move = null;
-                    move = console.readLine();
+                    //String move = null;
+                    //move = console.readLine();
+
+                    Noeud racine = new Noeud(board, true);
+                    Minmax minmax = new Minmax(racine);
+                    String move = minmax.findPositionChange(racine,2,true);
+
                     output.write(move.getBytes(),0,move.length());
                     output.flush();
 
